23827a6a371420d097bc29d0346e3b10ae44cdd9
SayinGonzalez <107142466+SayinGonzalez@users.noreply.github.com>
2022-11-17 13:51:50 -0300

    boton

--- Prueba/app.js
+++ Prueba/app.js
@@ -1,21 +1,40 @@
-//const button = document.getElementById('button')
+let data = [];
+function Requisitos(index) {
+    console.log(index)
+}
 
 axios({
-    method:'GET',
+    method: 'GET',
     url: 'http://170.210.142.11:1347/api/becas',
-    
+
 }).then(res => {
 
     console.log(res.data);
-    for(i=0; i<6; i++){
+    data = res.data;
+    for (i = 0; i < res.data.data.length; i++) {
         document.getElementById('datos').innerHTML
-            += "<h2>"+res.data.data[i].attributes.nombre+"</h2>" + 
-                "<p>"+res.data.data[i].attributes.comentario+"</p>" +
-                "<img src="+res.data.data[i].attributes.imag_url+">"
-                
+            += "<h2>" + res.data.data[i].attributes.nombre + "</h2>" +
+            "<p>" + res.data.data[i].attributes.comentario + "</p>" +
+            "<img src=" + res.data.data[i].attributes.imag_url + ">" + "<br>" +
+            "<button id='btn" + i + "'>Requisitos</button>" +
+            "<div id='req" + i + "'></div>"
+        // console.log()
+
+
     }
+    for (  i = 0; i < res.data.data.length; i++) {
+
+      let j = i
+        let c=res.data.data[i].attributes.requisitos[0].text
+        document.getElementById(`btn${i}`).addEventListener('click', () => {
+            document.getElementById(`req${j}`).innerHTML = "<p>"+c+"</p>"
+
+        })
+    }
+
 })
-.catch(err => console.log(err))
+
+    .catch(err => console.log(err))
 
 //button.addEventListener ('click', () => {
 //})
